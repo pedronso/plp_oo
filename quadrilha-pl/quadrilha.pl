62c5379434e907d47b19f551c9fab30036ed39c1
DENO <davy.eugenio@hotmail.com>
2023-05-23 00:25:42 -0300

    Programa em Prolog

---
+++ quadrilha-pl/quadrilha.pl
@@ -0,0 +1,50 @@
+:- discontiguous amava/2.
+
+aventureira(dora).
+
+amava(carlos, dora).
+amava(dora, lia).
+amava(lia, léa).
+amava(léa, paulo).
+amava(paulo, juca).
+amava(juca, dora).
+amava(dora, carlos).
+amava(carlos, dora).
+amava(dora, rita).
+amava(rita, dito).
+amava(dito, rita).
+amava(rita, carlos).
+amava(carlos, dora).
+amava(dora, pedro).
+amava(pedro, filha).
+amava(filha, carlos).
+amava(carlos, dora).
+
+amava(dora, carlos).
+amava(dora, lia).
+amava(dora, léa).
+amava(dora, paulo).
+amava(dora, juca).
+amava(dora, rita).
+amava(dora, dito).
+amava(dora, pedro).
+amava(dora, filha).
+
+genero(carlos, masculino).
+genero(dora, feminino).
+genero(lia, feminino).
+genero(léa, feminino).
+genero(paulo, masculino).
+genero(juca, masculino).
+genero(rita, feminino).
+genero(dito, masculino).
+genero(pedro, masculino).
+genero(filha, feminino).
+
+amadas(Y, X) :- 
+    amava(Y, X),
+    genero(X, feminino).
+
+amados(Y, X) :- 
+    amava(Y, X),
+    genero(X, maculino).
